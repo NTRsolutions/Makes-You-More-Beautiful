7327124f603917774a1f47f44c6ad0adf04a7f88
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-07-17 14:18:08 -0700

    xml modified

---
+++ app/src/main/java/com/example/nagi/makesyoubeautiful/ShareMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.nagi.makesyoubeautiful;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_menu);
+    }
+}
