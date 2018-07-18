97c581d99552cb8d50a462805a3fdecc07618ea6
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-07-18 12:41:45 -0700

    camera system added

--- app/src/main/java/com/example/nagi/makesyoubeautiful/FilterActivity.java
+++ app/src/main/java/com/example/nagi/makesyoubeautiful/FilterActivity.java
@@ -1,13 +1,51 @@
 package com.example.nagi.makesyoubeautiful;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FilterActivity extends AppCompatActivity {
+    public ImageView photoIv;
+    public ImageView smallFilterIv;
+    public ImageView homeButton;
+    public static final String BACK_HOME_KEY = "backhome";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        //ImageView of captured photo by camera
+        photoIv = findViewById(R.id.photoImageView);
+
+
+        smallFilterIv = findViewById(R.id.smallFilterPhoto);
+        homeButton = findViewById(R.id.homeButton);
+
+        smallFilterIv.setImageResource(R.drawable.filterphoto);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backhome = new Intent(FilterActivity.this,MenuActivity.class);
+                backhome.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(backhome);
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Bitmap image = (Bitmap) extras.get("image");
+            if (image != null) {
+                photoIv.setImageBitmap(image);
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/nagi/makesyoubeautiful/MenuActivity.java
+++ app/src/main/java/com/example/nagi/makesyoubeautiful/MenuActivity.java
@@ -1,13 +1,55 @@
 package com.example.nagi.makesyoubeautiful;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MenuActivity extends AppCompatActivity {
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public ImageView cameraButton;
+    public ImageView fileButton;
+//    public Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        cameraButton = findViewById(R.id.cameraButton);
+        fileButton = findViewById(R.id.fileButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraIntent.resolveActivity(getPackageManager())!= null){
+                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Intent imageIntent = new Intent(MenuActivity.this, FilterActivity.class);
+            imageIntent.putExtra("image", imageBitmap);
+            startActivity(imageIntent);
+
+        }
+    }
+
 }
